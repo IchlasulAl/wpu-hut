bd75e1e4847674a03feae544df8d9599159778e8
Ichlasul al <m.ichal7749@mail.unpas.ac.id>
2019-12-03 14:16:36 +0700

    menampilkan data di jason dengan foreach

--- latihan1.php
+++ latihan1.php
@@ -30,7 +30,7 @@ $menu = $menu["menu"];
             </button>
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div class="navbar-nav">
-                    <a class="nav-item nav-link active" href="#">Home</a>
+                    <a class="nav-item nav-link active" href="#">All Menu</a>
                 </div>
             </div>
         </div>
@@ -44,17 +44,19 @@ $menu = $menu["menu"];
         </div>
 
         <div class="row">
+            <?php foreach ($menu as $row):?>
             <div class="col-md-4">
-                <div class="card">
-                    <img src="img/menu/american-favourite.jpg" class="card-img-top" >
+                <div class="card mb-3">
+                    <img src="img/menu/<?= $row["gambar"];?>" class="card-img-top" >
                     <div class="card-body">
-                        <h5 class="card-title">American Favorite</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <h5 class="card-title">Rp. 60.000,-</h5>
+                        <h5 class="card-title"><?= $row["nama"];?></h5>
+                        <p class="card-text"><?= $row["deskripsi"];?></p>
+                        <h5 class="card-title"><?= $row["harga"];?></h5>
                         <a href="#" class="btn btn-primary">Pesan Sekarang</a>
                     </div>
                 </div>
             </div>
+            <?php endforeach?>
         </div>
 
 
